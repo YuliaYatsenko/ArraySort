30e77a8a19a24e8fe87202a9544a29fe9d9b6829
Joker <yuliaforewer@gmail.com>
2019-10-12 20:26:13 +0300

    Bubble sort + sum

--- src/main/java/project2/Main.java
+++ src/main/java/project2/Main.java
@@ -1,17 +1,18 @@
 package project2;
 import project2.sortingArray.Array;
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         run();
     }
     public static void run() {
-        int massiv[] = {45, 28, -10, 100, 0, 22};
+        int[] massiv = {45, 28, -10, 100, 0, 22};
         System.out.print("Unsorted array: ");
         for (int i = 0; i <= massiv.length - 1; i++) {
             System.out.print(massiv[i] + "  ");
-            }
-            System.out.println();
+        }
+        System.out.println();
         Array MyArray = new Array(massiv);
         MyArray.sortElement(massiv);
+        MyArray.sumElement(massiv);
     }
 }
--- src/main/java/project2/sortingArray/Array.java
+++ src/main/java/project2/sortingArray/Array.java
@@ -1,22 +1,30 @@
 package project2.sortingArray;
-
 public class Array {
     public int array[];
-
     public Array(int[] array) {
     this.array = array;
     }
     public void sortElement(int[] array) {
         System.out.print("Sorted array: " + " ");
-         for (int i = array.length-1; i >= 0; i--) {
-             for (int j = 0; j < array.length-1; j++) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < array.length - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
-             }
-             System.out.print(array[i]+"  ");
+            }
+            System.out.print(array[i] + "  ");
+            System.out.println();
         }
+    }
+    public int sumElement(int[] array) {
+        System.out.print("Sum of all elements: "+" ");
+        int sum=0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum);
+        return sum;
     }
 }
